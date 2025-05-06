6588717a1a3078a7e19c243db50097f854700a62
Joaquin Prada <joaquiprada@gmail.com>
2025-05-06 21:41:41 +0000

    fix mutex deadlock

--- src/keyRepository.ts
+++ src/keyRepository.ts
@@ -107,33 +107,30 @@ export class KeyRepository {
   }
 
   private async getValueDb(key: string, existing: boolean): Promise<Database> {
-    const valuedb = await this.mutex.runExclusive(async () => {
-      let valueDb: Database;
-      if (this.keyDbs.has(key)) {
-        // If we are replicating the key, we use the existing value database.
-        valueDb = this.keyDbs.get(key);
+    let valueDb: Database;
+    if (this.keyDbs.has(key)) {
+      // If we are replicating the key, we use the existing value database.
+      valueDb = this.keyDbs.get(key);
+    } else {
+      // If we are not replicating the key, we open the value database.
+      // TODO: Find a better protocol to name the valueDb, current protocol:
+      // "<keyDbName>::<ValueDbName>"
+      const valueDbName = `${this.dbName}::${key}`;
+      valueDb = new Database(valueDbName, this.orbitdb, this.events);
+      if (existing) {
+        // If the database already exists, we open it and sync it.
+        await valueDb.initExisting();
       } else {
-        // If we are not replicating the key, we open the value database.
-        // TODO: Find a better protocol to name the valueDb, current protocol:
-        // "<keyDbName>::<ValueDbName>"
-        const valueDbName = `${this.dbName}::${key}`;
-        valueDb = new Database(valueDbName, this.orbitdb, this.events);
-        if (existing) {
-          // If the database already exists, we open it and sync it.
-          await valueDb.initExisting();
-        } else {
-          await valueDb.initNew();
-        }
-
-        // TODO: The new database needs to stay accessible for the collaborators to replicate it.
-        //       Every change is made without confirmation that it was replicated to the collaborators.
-        //       To mitigate this we replicate permanently the keyDb. Asuming non collaborators are
-        //       temporary nodes, and could not stay too long in the network. See if this is the best
-        //       approach.
-        this.keyDbs.set(key, valueDb);
+        await valueDb.initNew();
       }
-      return valueDb;
-    });
-    return valuedb;
+
+      // TODO: The new database needs to stay accessible for the collaborators to replicate it.
+      //       Every change is made without confirmation that it was replicated to the collaborators.
+      //       To mitigate this we replicate permanently the keyDb. Asuming non collaborators are
+      //       temporary nodes, and could not stay too long in the network. See if this is the best
+      //       approach.
+      this.keyDbs.set(key, valueDb);
+    }
+    return valueDb;
   }
 }
